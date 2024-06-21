9d7b108526eefe4d4031bd9ca4244f76e22bca05
manishsaraan <manish021js@gmail.com>
2024-06-21 17:55:13 +0530

    create bookmarks id context

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,9 +1,23 @@
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { useBookmarkIdsContext } from "../contexts/BookmarkContextProvider";
 
-export default function BookmarkIcon() {
+type BookmarkIconProps = {
+  id: number;
+};
+export default function BookmarkIcon({ id }: BookmarkIconProps) {
+  const { handleToggleBookmark, bookmarkIds } = useBookmarkIdsContext();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+    e.preventDefault();
+    handleToggleBookmark(id);
+  };
+
   return (
-    <button className="bookmark-btn">
-      <BookmarkFilledIcon className="filled" />
+    <button onClick={handleClick} className="bookmark-btn">
+      <BookmarkFilledIcon
+        className={`${bookmarkIds.includes(id) && "filled"}`}
+      />
     </button>
   );
 }
---
+++ src/contexts/BookmarkContextProvider.tsx
@@ -0,0 +1,45 @@
+import { useState, createContext, useContext } from "react";
+
+type BookmarkContextProp = {
+  handleToggleBookmark: (id: number) => void;
+  bookmarkIds: number[];
+};
+const BookmarkContext = createContext<BookmarkContextProp | null>(null);
+
+export default function BookmarkContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [bookmarkIds, setBookmarkIds] = useState<number[]>([]);
+
+  const handleToggleBookmark = (id: number) => {
+    if (bookmarkIds.includes(id)) {
+      setBookmarkIds((prev) => prev.filter((item) => item !== id));
+    } else {
+      setBookmarkIds((prev) => [...prev, id]);
+    }
+  };
+
+  return (
+    <BookmarkContext.Provider
+      value={{
+        bookmarkIds,
+        handleToggleBookmark,
+      }}
+    >
+      {children}
+    </BookmarkContext.Provider>
+  );
+}
+
+export const useBookmarkIdsContext = () => {
+  const ctx = useContext(BookmarkContext);
+
+  if (!ctx) {
+    throw new Error("Bookmark Context is missing");
+  }
+
+  const { handleToggleBookmark, bookmarkIds } = ctx;
+  return { handleToggleBookmark, bookmarkIds };
+};
